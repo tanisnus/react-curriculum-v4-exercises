7e8c3ea8ce43fea3f8bd76a8b2475af529c51c31
tanisnus <taniss246@gmail.com>
2026-04-06 18:46:25 -0700

    Added-Exercise 1

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,25 @@
 
 export default function StudentWork() {
   //add variables here
+  const name = 'Tanis Sarbatananda';
+  const age = 24;
+  const hobbies = ['Music', 'Working Out', 'Video Games', 'Soccer'];
   return (
     <div>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1> About Me</h1>
+      <p>
+        {' '}
+        My name is {name}. My interestes include web design and web development.
+        By the end of 2026, I want to create at least 3 fully functional
+        websites that impact real users.
+      </p>
+      <h2> My Hobbies</h2>
+      <ul>
+        {hobbies.map((hobby) => (
+          <li key={hobby}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
