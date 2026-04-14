394209c2ee7ba64117a6b832f9b1a88c1a62ea9c
tanisnus <taniss246@gmail.com>
2026-04-13 17:51:37 -0700

    Added - Lesson 2 Exercise

---
+++ src/exercises/lesson-02/SnackList.jsx
@@ -0,0 +1,23 @@
+function SnackList() {
+  const mySnackList = [
+    { name: 'Apple Slices', rank: 5 },
+    { name: 'Doritos', rank: 4 },
+    { name: 'Pringles', rank: 3 },
+    { name: 'Lays', rank: 2 },
+    { name: 'Pocky', rank: 1 },
+  ];
+
+  const sortedSnackList = mySnackList.toSorted((a, b) => a.rank - b.rank);
+
+  return (
+    <div>
+      <ol>
+        {sortedSnackList.map((snack) => (
+          <li key={snack.name}>{snack.name}</li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default SnackList;
