d783d837c1942082eb01e2cd0bfa5d988a17402c
tanisnus <taniss246@gmail.com>
2026-04-28 17:30:01 -0700

    lesson 4 - submission

--- src/exercises/lesson-04/FillRefFocus.jsx
+++ src/exercises/lesson-04/FillRefFocus.jsx
@@ -1,15 +1,26 @@
 // TOPIC: Correct useRef usage to control DOM elements
 // TASK: Implement focusing an input field when the button is clicked.
+
+import { useRef } from 'react';
 export default function FillRefFocus() {
-  function focusInput() {}
+  const inputRef = useRef(null);
+  function focusInput() {
+    inputRef.current.focus();
+  }
 
   return (
     <div>
       <h2>useRef: Focusing an Input</h2>
 
-      <input type="text" placeholder="Type here..." />
+      <input type="text" placeholder="Type here..." ref={inputRef} />
 
       <button onClick={focusInput}>Focus Input</button>
     </div>
   );
 }
+
+// Explanation:
+// Issue: The input field is not focused when the button is clicked.
+// Solution:
+// - Create a ref object and pass it as a prop to the input field's ref attribute.
+// - Define the focusInput function by calling the focus() method on the DOM node referenced by the ref object.
--- src/exercises/lesson-04/FindCorrectHook.jsx
+++ src/exercises/lesson-04/FindCorrectHook.jsx
@@ -1,16 +1,26 @@
 // TOPIC: Choose the correct tool: useRef vs useState
 // TASK: Make sure it updates the text *without* triggering a re-render
+import { useRef } from 'react';
 export default function FindCorrectHook() {
-  let clickCount = 0; // ← incorrect implementation
+  const clickCountRef = useRef(0);
 
-  function handleClick() {
-    clickCount++;
+  function handleClick(event) {
+    clickCountRef.current++;
+    event.target.textContent = `${clickCountRef.current} Clicks`;
   }
 
   return (
     <div>
       <h2>useRef vs useState Decision</h2>
-      <button onClick={handleClick}>{clickCount} Clicks</button>
+      <button onClick={handleClick}> Clicks</button>
     </div>
   );
 }
+
+// Explanation:
+
+// Issue: The click count is not updating correctly.
+// Solution: using useRef because making updates does not trigger a re-render.
+// useRef is used to store a mutable value, which is the click count in this case.
+// useRef does not cause a re-render when the value changes.
+// Accessing and updating the current value of the ref with .current
